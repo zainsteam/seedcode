8b67c6af37e2c835917c2ceb275e1bc9065971ec
zainsteam <zains.team00@hotmail.com>
2020-04-14 17:33:37 +0500

    last 3 pages left

--- js/script.js
+++ js/script.js
@@ -27,3 +27,25 @@ function openNav() {
       });
     }, false);
   })();
+
+ // Get the container element
+var btnContainer = document.getElementById("myDIV");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("boxText");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    var icon = (current[0].getElementsByTagName("i"));
+    
+    // If there's no active class
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+    
+    // Add the active class to the current/clicked button
+    this.className += " active";
+  });
+}
